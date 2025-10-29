ff1916628742d157dbd907cfeee076d16f3f3ed6
Katherine James-Gzyl <katherine.james-gzyl@agr.gc.ca>
2025-10-29 11:11:11 -0400

    Update snk_report.sh for full test run configuration and Snakemake version upgrade

--- snk_report.sh
+++ snk_report.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=dry_run
-#SBATCH --output=dry_run_%j.out
-#SBATCH --error=dry_run_%j.err
+#SBATCH --job-name=full_test_run_report
+#SBATCH --output=%x_%A_%a.out
+#SBATCH --error=%x_%A_%a.err
 #SBATCH --cluster=gpsc8
 #SBATCH --partition=standard
 #SBATCH --account=aafc_aac
@@ -10,13 +10,8 @@
 
 source /gpfs/fs7/aafc/common/miniforge/miniforge3/etc/profile.d/conda.sh
 
-conda activate snakemake-9.6.0
+conda activate snakemake-9.9.0
 export PATH="$PWD/bin:$PATH"
 
-snakemake \
-    --profile /gpfs/fs7/aafc/projects/J-003165_abcc_rcba/code/metatranscriptomics-snakemake/profiles/slurm \
-    --configfile /gpfs/fs7/aafc/projects/J-003165_abcc_rcba/code/metatranscriptomics-snakemake/config/config.yaml \
-    --conda-prefix /gpfs/fs7/aafc/scratch/kjg000/tmpdir/metatranscriptomics_conda \
-    --printshellcmds \
-    --keep-going \
-    --report /gpfs/fs7/aafc/projects/J-003165_abcc_rcba/code/metatranscriptomics-snakemake/metatranscriptomics_report.html 
+#Can just run this from the head node.
+snakemake --report /gpfs/fs7/aafc/projects/J-003165_abcc_rcba/code/metatranscriptomics-snakemake/results/metatranscriptomics_report.html
